b2b548b9c0e2331ded27c4d4cec6955e272db747
Jieqiang <Jieqiang@JIEQIANG>
2017-05-02 07:21:46 +0800

    事件发布

---
+++ publisherDemo/IConsumer.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace publisherDemo
+{
+    /// <summary>
+    /// 消费者
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    public interface IConsumer<T>
+    {
+        void HandlerEvent(T eventMessage);
+    }
+}
---
+++ publisherDemo/IEventPublisher.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace publisherDemo
+{
+    public interface IEventPublisher
+    {
+        void Publish<T>(T eventMessage);
+    }
+}
